91756a645a2ae41a9ed49f573ac960792b68c50f
Fatemeh Soleimani <fwtemeh.soleimanii@gmail.com>
2024-06-04 23:48:55 -0800

    add total curent week report to dash

--- src/features/dashboard/TaskReport.jsx
+++ src/features/dashboard/TaskReport.jsx
@@ -1,17 +1,29 @@
-import { format } from 'date-fns';
-import Spinner from '../../components/Spinner';
-import useGetTask from '../schedule/useGetTask';
+import React from 'react'
+import { format } from 'date-fns'
+import Spinner from '../../components/Spinner'
+import useGetTask from '../schedule/useGetTask'
+import { useSelector } from 'react-redux'
 
 function TaskReport() {
-  const { data: task, isLoading: isTask } = useGetTask();
-  
-  if (isTask) return <Spinner />;
+  const { data: task, isLoading: isTask } = useGetTask()
+  const { GroupDataTimerArray, taskNames, weekStartDates } = useSelector(
+    (store) => store.timer
+  )
 
-  const total = task.length || 0;
-  const todoTask = task?.filter(task => task.status?.toString() === 'Not Started').length || 0;
-  const inProgressTask = task?.filter(task => task.status?.toString() === 'In Progress').length || 0;
-  const completedTask = task.filter(task => task.status?.toString() === 'Completed').length || 0;
-  const allTimer = task.reduce((acc, cur) => acc + cur.duration, 0) || 0;
+  if (isTask) return <Spinner />
+
+  const total = task?.length || 0
+  const todoTask = task?.filter((task) => task.status?.toString() === 'Not Started').length || 0
+  const inProgressTask = task?.filter((task) => task.status?.toString() === 'In Progress').length || 0
+  const completedTask = task?.filter((task) => task.status?.toString() === 'Completed').length || 0
+  const allTimer = task?.reduce((acc, cur) => acc + cur.duration, 0) || 0
+
+  // Flatten the nested groups array and calculate the total duration
+  const totalWeekTimer = weekStartDates?.[0]?.groups
+    .flat()
+    .reduce((acc, cur) => acc + cur.duration, 0) || 0
+
+  const formattedTotalWeekTimer = new Date(totalWeekTimer * 1000).toISOString().substr(11, 8)
 
   return (
     <div className='w-full p-2 md:p-4'>
@@ -33,12 +45,12 @@ function TaskReport() {
           <h1 className='text-sm md:text-lg text-gray-600'>Completed</h1>
         </div>
         <div className='bg-white p-2 md:p-4 rounded-lg shadow-md text-center hover:shadow-lg transition-shadow flex-1 min-w-[120px] md:min-w-[150px]'>
-          <span className='text-lg md:text-2xl text-orange-500'>{new Date(allTimer * 1000).toISOString().substr(11, 8)}</span>
-          <h1 className='text-sm md:text-lg text-gray-600'>Total Task Timer</h1>
+          <span className='text-lg md:text-2xl text-orange-500'>{formattedTotalWeekTimer}</span>
+          <h1 className='text-sm md:text-lg text-gray-600'> Current Week Timer</h1>
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default TaskReport;
+export default TaskReport
