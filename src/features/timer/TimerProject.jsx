6afcec97f6d832cc8eebec882a4ced021f082fa7
Fatemeh Soleimani <fwtemeh.soleimanii@gmail.com>
2024-06-05 10:19:44 -0800

    safari check

--- src/features/timer/TimerProject.jsx
+++ src/features/timer/TimerProject.jsx
@@ -7,12 +7,7 @@ import useEditTimer from './useEditTimer'
 
 import Spinner from '../../components/Spinner'
 import { formatDate, formatTime } from '../../helpers/TimeConverter'
-import {
-  startOfDay,
-  startOfWeek,
-  endOfWeek,
-  isThisWeek,
-} from 'date-fns'
+import { startOfDay, startOfWeek, endOfWeek, isThisWeek } from 'date-fns'
 import {
   addGroupDataTimerArray,
   updateTaskName,
@@ -88,99 +83,103 @@ function TimerProject() {
   }
 
   if (isLoading || isEdit) return <Spinner />
-  
+
   return (
     <div className='w-[98%] mx-auto mb-8'>
       {weekStartDates.length > 0 && (
         <div className='mt-6 w-full h-full'>
-          {weekStartDates.map(
-            ({ weekStart, weekEnd, groups }, index) => (
-              <div key={`week-${index}`} className='mb-8'>
-                <div className='flex flex-col m-2 gap-1'>
-                  <div className='text-lg font-bold text-gray-600'>{weekStart} - {isThisWeek(new Date(groups[0][0].created_at)) ? 'Today' : weekEnd}</div>
-                  <div className='flex items-center gap-1 text-sm text-gray-500'>
-                    <span>Total : </span>
-                    <span className='font-semibold text-gray-700'>
-                      {new Date(
-                        groups.reduce((acc, group) => {
-                          return (
-                            acc +
-                            group.reduce(
-                              (groupAcc, cur) => groupAcc + cur.duration,
-                              0
-                            )
+          {weekStartDates.map(({ weekStart, weekEnd, groups }, index) => (
+            <div key={`week-${index}`} className='mb-8'>
+              <div className='flex  m-2 gap-1'>
+              
+                <div className='text-md text-gray-600'>
+                  {weekStart} -{' '}
+                  {isThisWeek(new Date(groups[0][0].created_at))
+                    ? 'Today'
+                    : weekEnd}
+                </div>|
+                <div className='flex fle items-center gap-1 text-sm text-gray-500'>
+                  <span>Total : </span>
+                  <span className='font-semibold text-gray-700'>
+                    {new Date(
+                      groups.reduce((acc, group) => {
+                        return (
+                          acc +
+                          group.reduce(
+                            (groupAcc, cur) => groupAcc + cur.duration,
+                            0
                           )
-                        }, 0) * 1000
-                      )
-                        .toISOString()
-                        .substr(11, 8)}
-                    </span>
+                        )
+                      }, 0) * 1000
+                    )
+                      .toISOString()
+                      .substr(11, 8)}
+                  </span>
+                </div>
+              </div>
+
+              {groups.map((group, groupIndex) => (
+                <div
+                  key={`group-${groupIndex}`}
+                  className='shadow-lg bg-white h-auto flex flex-col rounded-xl mt-3 ml-2'
+                >
+                  <div className='flex flex-col bg-blue-100 w-full rounded-[1rem] shadow-sm'>
+                    <div className='flex justify-between p-4 items-center bg-blue-200 rounded-t-[1rem]'>
+                      <span className='text-gray-700 font-semibold'>
+                        {formatDate(new Date(group[0].created_at), 'MM/dd')}
+                      </span>
+                      <span className='text-gray-900 text-xl font-bold'>
+                        {new Date(
+                          group.reduce(
+                            (acc, timer) => acc + timer.duration,
+                            0
+                          ) * 1000
+                        )
+                          .toISOString()
+                          .substr(11, 8)}
+                      </span>
+                    </div>
+
+                    {group.map((timerToday, idx) => (
+                      <div
+                        key={timerToday.id}
+                        className={`bg-white border-b flex items-center justify-between w-full gap-5 p-4 ${
+                          idx === group.length - 1 ? 'rounded-b-[1rem]' : ''
+                        }`}
+                      >
+                        <Input
+                          variant='bordered'
+                          size='lg'
+                          className='w-1/3 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500'
+                          type='text'
+                          value={taskNames[timerToday.id] || ''}
+                          onChange={(e) => {
+                            handleInputChange(timerToday.id, e.target.value)
+                          }}
+                          onBlur={(e) =>
+                            handleEditInput(timerToday.id, e.target.value)
+                          }
+                        />
+                        <div className='flex items-center gap-5'>
+                          <div className='flex justify-end items-center gap-1 text-gray-700'>
+                            <span>{formatTime(timerToday.startTime)}</span>
+                            <span>-</span>
+                            <span>{formatTime(timerToday.endTime)}</span>
+                          </div>
+                          <span className='text-gray-700'>
+                            {new Date(timerToday.duration * 1000)
+                              .toISOString()
+                              .substr(11, 8)}
+                          </span>
+                          <TimerProjectSettings id={timerToday.id} />
+                        </div>
+                      </div>
+                    ))}
                   </div>
                 </div>
-
-                {groups.map((group, groupIndex) => (
-                  <div
-                    key={`group-${groupIndex}`}
-                    className='shadow-lg bg-white h-auto flex flex-col rounded-xl mt-3 ml-2'
-                  >
-                    <div className='flex flex-col bg-blue-100 w-full rounded-[1rem] shadow-sm'>
-                      <div className='flex justify-between p-4 items-center bg-blue-200 rounded-t-[1rem]'>
-                        <span className='text-gray-700 font-semibold'>
-                          {formatDate(new Date(group[0].created_at), 'MM/dd')}
-                        </span>
-                        <span className='text-gray-900 text-xl font-bold'>
-                          {new Date(
-                            group.reduce(
-                              (acc, timer) => acc + timer.duration,
-                              0
-                            ) * 1000
-                          )
-                            .toISOString()
-                            .substr(11, 8)}
-                        </span>
-                      </div>
-
-                      {group.map((timerToday, idx) => (
-                        <div
-                          key={timerToday.id}
-                          className={`bg-white border-b flex items-center justify-between w-full gap-5 p-4 ${
-                            idx === group.length - 1 ? 'rounded-b-[1rem]' : ''
-                          }`}
-                        >
-                          <Input
-                            variant='bordered'
-                            size='lg'
-                            className='w-1/3 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500'
-                            type='text'
-                            value={taskNames[timerToday.id] || ''}
-                            onChange={(e) => {
-                              handleInputChange(timerToday.id, e.target.value)
-                            }}
-                            onBlur={(e) =>
-                              handleEditInput(timerToday.id, e.target.value)
-                            }
-                          />
-                          <div className='flex items-center gap-5'>
-                            <div className='flex justify-end items-center gap-1 text-gray-700'>
-                              <span>{formatTime(timerToday.startTime)}</span>
-                              <span>-</span>
-                              <span>{formatTime(timerToday.endTime)}</span>
-                            </div>
-                            <span className='text-gray-700'>
-                              {new Date(timerToday.duration * 1000)
-                                .toISOString()
-                                .substr(11, 8)}
-                            </span>
-                            <TimerProjectSettings id={timerToday.id} />
-                          </div>
-                        </div>
-                      ))}
-                    </div>
-                  </div>
-                ))}
-              </div>
-            )
-          )}
+              ))}
+            </div>
+          ))}
         </div>
       )}
     </div>
