12d61a318dd2f6371ccef54f5d97e65d67fd62da
Fatemeh Soleimani <fwtemeh.soleimanii@gmail.com>
2024-09-23 14:42:21 +0330

    edit profile page

--- src/components/Myprofile.jsx
+++ src/components/Myprofile.jsx
@@ -13,7 +13,7 @@ const MyProfile = () => {
   const [avatarUrl, setAvatarUrl] = useState('')
 
   const { mutate: editProf, isLoading: isUpdating } = useUpdateUser()
-  
+
   useEffect(() => {
     if (user) {
       setUsername(user.user_metadata?.username || '')
@@ -55,59 +55,63 @@ const MyProfile = () => {
         avatar: avatarUrlToSave,
       })
     }
-    if (!avatar){
+    if (!avatar) {
       editProf({
         username,
         password,
-      })}
+      })
+    }
   }
 
   if (isUserLoading || isUpdating) return <Spinner />
 
   return (
     <>
-    <div className='pl-[7rem] py-'>
-      <h1 className='text-3xl  font-semibold mb-6'>My Profile</h1>
-      <div className='flex flex-col md:flex-row gap-8 pr-8 pl-4'>
-        <div className='flex-shrink-0 relative gap-4'>
-          <Avatar src={avatarUrl} className='w-20 h-20 text-large' />
-          <label
-            htmlFor='avatarInput'
-            className='absolute md:w-[8rem] text-center  md:left-[18rem]  bottom-0 right-0 bg-primary hover:bg-primary-dark py-2 rounded-xl text-white cursor-pointer'
-          >
-            Choose File
-          </label>
-          <input
-            id='avatarInput'
-            type='file'
-            accept='image/*'
-            className='hidden'
-            onChange={handleAvatarChange}
-          />
-        </div>
-        <div className='flex-grow lg:ml-[2rem]'>
-          <Input
-            label='Username'
-            placeholder='Enter your username'
-            value={username}
-            onChange={handleUsernameChange}
-            className='mb-4'
-          />
-          <Input
-            type='password'
-            label='Password'
-            placeholder='Enter your password'
-            value={password}
-            onChange={handlePasswordChange}
-            className='mb-4'
-          />
-          <Button className='text-lg' onClick={handleSave} color='primary'>
-            Save Changes
-          </Button>
+      <div className='lg:pl-[7rem] pl-4 h-full'>
+        <h1 className='text-3xl  font-semibold mb-6'>My Profile</h1>
+        <div className='flex h-full flex-col md:flex-row gap-8 pr-8 pl-4'>
+          <div className='flex-shrink-0 relative gap-4 bf'>
+            <Avatar src={avatarUrl} className='w-20 h-20 text-large' />
+          </div>
+          <div className='flex-grow lg:ml-[2rem]'>
+            <Input
+              label='Username'
+              placeholder='Enter your username'
+              value={username}
+              onChange={handleUsernameChange}
+              className='mb-4'
+            />
+            <Input
+              type='password'
+              label='Password'
+              placeholder='Enter your password'
+              value={password}
+              onChange={handlePasswordChange}
+              className='mb-4'
+            />
+            <div className='flex gap-4'>
+              <label
+                htmlFor='avatarInput'
+                className='w-[8rem] text-center md:left-[18rem] top-0  md:top-0 right-0 bg-primary hover:bg-primary-dark py-2 rounded-xl text-white cursor-pointer'
+              >
+                Choose File
+              </label>
+              <input
+                id='avatarInput'
+                type='file'
+                accept='image/*'
+                className='hidden'
+                onChange={handleAvatarChange}
+              />
+              <Button className='text-lg' onClick={handleSave} color='primary'>
+                Save Changes
+              </Button>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
- </> )
+    </>
+  )
 }
 
 export default MyProfile
