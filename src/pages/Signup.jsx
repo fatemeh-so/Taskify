3515b07c5ec88edc5b4e1366b308b33534289cdf
Fatemeh Soleimani <fwtemeh.soleimanii@gmail.com>
2024-11-25 11:59:50 +0330

    update ui

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -29,7 +29,7 @@ function SignUp() {
     <div className='min-h-screen flex items-center justify-center bg-gradient-to-br from-pink-400 to-pink-50 p-4'>
       <div className='bg-white rounded-lg shadow-md p-8 w-full max-w-md'>
         <h1 className='text-2xl font-semibold text-center text-gray-800 mb-6'>
-          Create an Account on Taskify
+          Create an Account
         </h1>
 
         <form onSubmit={handleSubmit(onSubmit)} className='space-y-4'>
