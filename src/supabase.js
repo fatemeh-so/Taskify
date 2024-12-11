39929ba6e89b4132fb622f1697f0b84cd2a7b984
Fatemeh Soleimani <fwtemeh.soleimanii@gmail.com>
2024-12-11 21:44:27 +0330

    db

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 // src/supabaseClient.js
 import { createClient } from '@supabase/supabase-js'
 
-export const supabaseUrl = import.meta.env.VITE_APP_SUPABASE_URL
+export const supabaseUrl = 'https://ipujmkdynjpwmrbuoulr.supabase.co'
 const supabaseKey = import.meta.env.VITE_APP_SUPABASE_KEY
 
 const supabase = createClient(supabaseUrl, supabaseKey)
