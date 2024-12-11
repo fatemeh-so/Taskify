be5a2d85439678bfb6e04d8961c08482d91e0703
Fatemeh Soleimani <fwtemeh.soleimanii@gmail.com>
2024-12-11 21:45:49 +0330

    db

--- src/supabase.js
+++ src/supabase.js
@@ -2,7 +2,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabaseUrl = 'https://ipujmkdynjpwmrbuoulr.supabase.co'
-const supabaseKey = import.meta.env.VITE_APP_SUPABASE_KEY
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlwdWpta2R5bmpwd21yYnVvdWxyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYzMTMyMjEsImV4cCI6MjAzMTg4OTIyMX0.hubTO6Ib0rXbZ3FeDkqi2P7pUUp1sgGx_ii_Kbkv39M'
 
 const supabase = createClient(supabaseUrl, supabaseKey)
 
