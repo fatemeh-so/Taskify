42a1bb4105091f6752ac64f67f46f4acd7aabb21
Fatemeh Soleimani <fwtemeh.soleimanii@gmail.com>
2025-10-11 13:54:03 +0330

    Update PriorityTasksChart.jsx

--- src/features/dashboard/PriorityTasksChart.jsx
+++ src/features/dashboard/PriorityTasksChart.jsx
@@ -1,60 +1,96 @@
-import { PieChart, Pie, Cell, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-import Spinner from '../../components/Spinner';
-import useGetTask from '../schedule/useGetTask';
-import { useMemo } from 'react';
+import {
+  PieChart,
+  Pie,
+  Cell,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from 'recharts'
+import { useMemo } from 'react'
+import { useTranslation } from 'react-i18next'
 
-function PriorityTaskCharts() {
-  const COLORS = ['#7CB342', '#FFB300', '#F06292', '#4DD0E1', '#9575CD', '#FF7043', '#81C784', '#4DB6AC', '#64B5F6', '#A1887F'];
+function ProrityTaskCharts({ tasks }) {
+  const { t } = useTranslation() // مطمئن شوید که این درست کار می‌کند
+  const COLORS = [
+    '#7CB342',
+    '#FFB300',
+    '#F06292',
+    '#4DD0E1',
+    '#9575CD',
+    '#FF7043',
+    '#81C784',
+    '#4DB6AC',
+    '#64B5F6',
+    '#A1887F',
+  ]
 
-  const { data: tasks, isLoading: isTask } = useGetTask();
+  const totalTasks = tasks.length
 
-  if (isTask) return <Spinner />;
-  
-  const totalTasks = tasks.length;
-
-  const taskPriorities = useMemo(() => {
-    const priorities = {};
+  const taskCategories = useMemo(() => {
+    const categories = {}
     if (tasks) {
       tasks.forEach((task) => {
-        if (priorities[task.priority]) {
-          priorities[task.priority]++;
+        if (categories[task.category]) {
+          categories[task.category]++
         } else {
-          priorities[task.priority] = 1;
+          categories[task.category] = 1
         }
-      });
+      })
     }
-    return Object.entries(priorities).map(([name, value]) => ({
-      name,
+    return Object.entries(categories).map(([name, value]) => ({
+      name: t(name),
       value,
       percentage: ((value / totalTasks) * 100).toFixed(0), // Calculate percentage without decimal places
-    }));
-  }, [tasks, totalTasks]);
+    }))
+  }, [tasks, totalTasks])
+
+  if (tasks.length === 0) {
+    return (
+      <div className='flex mt-4 h-[17.3rem] shadow-sm rounded-lg justify-center items-center bg-white w-full text-gray-500'>
+        {t('noTask')}
+      </div>
+    )
+  }
 
   return (
-    <div className="mt-8 bg-white p-4 rounded-lg shadow-md">
-      <h2 className="text-lg font-bold text-gray-800 mb-4">Task Priorities</h2>
-      <ResponsiveContainer width="100%" height={200} minHeight={150} maxHeight={300}>
+    <div className='mt-4 bg-white p-4 rounded-lg shadow-md'>
+      <h2 className='text-lg text-gray-800 mb-4'>{t('taskCategories')}</h2>
+      <ResponsiveContainer
+        width='100%'
+        height={200}
+        minHeight={150}
+        maxHeight={300}
+      >
         <PieChart>
           <Pie
-            data={taskPriorities}
-            dataKey="value"
-            nameKey="name"
-            cx="50%"
-            cy="50%"
+            data={taskCategories}
+            dataKey='value'
+            nameKey='name'
+            cx='50%'
+            cy='50%'
             outerRadius={60}
-            fill="#8884d8"
+            fill='#8884d8'
             label={({ name, percentage }) => `${percentage}%`} // Display percentage inside the Pie
           >
-            {taskPriorities.map((entry, index) => (
-              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+            {taskCategories.map((entry, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={COLORS[index % COLORS.length]}
+              />
             ))}
           </Pie>
           <Tooltip />
-          <Legend layout="vertical" align="right" verticalAlign="middle" radius="100%" className='rounded-full' />
+          <Legend
+            layout='vertical'
+            align='right'
+            verticalAlign='middle'
+            radius='100'
+            className='rounded-full'
+          />
         </PieChart>
       </ResponsiveContainer>
     </div>
-  );
+  )
 }
 
-export default PriorityTaskCharts;
+export default ProrityTaskCharts
