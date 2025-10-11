b2390a396432330dced47620e4db9c1e6e28102b
Fatemeh Soleimani <fwtemeh.soleimanii@gmail.com>
2025-10-11 13:48:06 +0330

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,9 +1,9 @@
 // src/supabaseClient.js
 import { createClient } from '@supabase/supabase-js'
 
-export const supabaseUrl = 'https://ipujmkdynjpwmrbuoulr.supabase.co'
+export const supabaseUrl = 'https://fhpqywvagrtfkizejhpf.supabase.co'
 const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlwdWpta2R5bmpwd21yYnVvdWxyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYzMTMyMjEsImV4cCI6MjAzMTg4OTIyMX0.hubTO6Ib0rXbZ3FeDkqi2P7pUUp1sgGx_ii_Kbkv39M'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZocHF5d3ZhZ3J0ZmtpemVqaHBmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI2MDg2NDQsImV4cCI6MjA2ODE4NDY0NH0.d3nEPWk6OJ1_srGAYQo7NV_CPSiNDjII-I5nAyHRh6Y'
 
 const supabase = createClient(supabaseUrl, supabaseKey)
 
