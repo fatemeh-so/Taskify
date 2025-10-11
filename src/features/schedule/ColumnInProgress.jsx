523134fe08b6ed48a5a3990be2243d4740315c73
Fatemeh Soleimani <fwtemeh.soleimanii@gmail.com>
2025-10-11 13:51:54 +0330

    Update ColumnInProgress.jsx ui

--- src/features/schedule/ColumnInProgress.jsx
+++ src/features/schedule/ColumnInProgress.jsx
@@ -2,13 +2,10 @@
 // Column.js
 import SquareRow from './SquareRow'
 
-
-function ColumnInProgress({ label, color,task }) {
-
+function ColumnInProgress({ label, color, task }) {
   // Check if task and task.status are defined before calling toString
   const InProgressTask =
     task?.filter((task) => task?.status?.toString() === 'In Progress') || []
-
 
   const bgColorClass =
     {
@@ -25,15 +22,21 @@ function ColumnInProgress({ label, color,task }) {
       <div className='flex justify-center items-center'>
         <div
           className={`text-center hidden md:block w-full rounded-[.4rem] pt-2 h-[2.5rem] font-bold mb-4 ${bgColorClass}`}
-          >
+        >
           {label}
         </div>
       </div>
-      <div className=' overflow-y-auto h-[100%] w-[100%] '>
-        {InProgressTask.map((task) => {
-          return <SquareRow key={task.id} task={task} />
-        })}
-      </div>
+      {InProgressTask.length > 0 ? (
+        <div className=' overflow-y-auto h-[100%] w-[100%] '>
+          {InProgressTask.map((task) => {
+            return <SquareRow key={task.id} task={task} />
+          })}
+        </div>
+      ) : (
+        <div className='flex justify-center items-center w-full h-24 text-gray-500 bg-[#ffffff] rounded-xl '>
+          No Task
+        </div>
+      )}
     </div>
   )
 }
