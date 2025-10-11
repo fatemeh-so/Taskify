d5a4c6b4a1c1ee82919dbb9ca35c33a4cae540aa
Fatemeh Soleimani <fwtemeh.soleimanii@gmail.com>
2025-10-11 13:50:41 +0330

    Update ColumnCopmleted.jsx ui

--- src/features/schedule/ColumnCopmleted.jsx
+++ src/features/schedule/ColumnCopmleted.jsx
@@ -1,15 +1,12 @@
 /* eslint-disable react/prop-types */
 import SquareRow from './SquareRow'
 
-
-function ColumnCompleted({ label, color,task }) {
+function ColumnCompleted({ label, color, task }) {
   // const { data: task, isLoading: isTask } = useGetTask()
 
   // Check if task and task.status are defined before calling toString
   const CompletedTask =
     task?.filter((task) => task?.status?.toString() === 'Completed') || []
-
-
 
   const bgColorClass =
     {
@@ -26,15 +23,21 @@ function ColumnCompleted({ label, color,task }) {
       <div className='flex justify-center items-center'>
         <div
           className={`text-center hidden md:block w-full rounded-[.4rem] pt-2 h-[2.5rem] font-bold mb-4 ${bgColorClass}`}
-          >
+        >
           {label}
         </div>
       </div>
-      <div className=' overflow-y-auto h-[100%] w-[100%] '>
-        {CompletedTask?.map((task) => {
-          return <SquareRow key={task?.id} task={task} />
-        })}
-      </div>
+      {CompletedTask.length > 0 ? (
+        <div className=' overflow-y-auto h-[100%] w-[100%] '>
+          {CompletedTask?.map((task) => {
+            return <SquareRow key={task?.id} task={task} />
+          })}
+        </div>
+      ) : (
+        <div className='flex justify-center items-center w-full h-24 text-gray-500 bg-[#ffffff] rounded-xl '>
+          No Task
+        </div>
+      )}
     </div>
   )
 }
